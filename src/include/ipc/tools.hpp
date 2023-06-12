319c09d3f335bb9c7bf5331a1ff64b697ed7c447
AR-CADE <armelcadetpetit@gmail.com>
2023-06-12 15:02:00 +0200

    Minor optimization in the ipc_tools::split_string method

--- src/include/ipc/tools.hpp
+++ src/include/ipc/tools.hpp
@@ -87,12 +87,11 @@ class ipc_tools
         const std::string & delimiter)
     {
         size_t pos_start = 0, pos_end, delim_len = delimiter.length();
-        std::string token;
         std::vector<std::string> res;
 
         while ((pos_end = s.find(delimiter, pos_start)) != std::string::npos)
         {
-            token     = s.substr(pos_start, pos_end - pos_start);
+            std::string token = s.substr(pos_start, pos_end - pos_start);
             pos_start = pos_end + delim_len;
             res.push_back(token);
         }
